cb6e2be2dbbc2569666ca5a4285ca235ef3daae0
Hassan <su7997221@gmiail.com>
2025-09-26 10:09:04 +0800

    添加 .gitignore 文件，更新 requirements.txt 以移除版本限制，新增 stltovoxel.py 文件

---
+++ stltovoxel.py
@@ -0,0 +1,3 @@
+import stltovoxel
+# stltovoxel.convert_file('combined.stl', 'combined\combined.png', resolution = 20000, voxel_size = 0.01)
+stltovoxel.convert_file('bubbly_flow.stl', 'combined\combined.png', resolution = 500)
